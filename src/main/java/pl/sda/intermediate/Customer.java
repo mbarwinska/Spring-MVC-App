b99e71cd31355b283cb1cbf4afc068e948c6fa3a
Maja Barwińska <majakempa1@gmail.com>
2019-10-26 12:49:21 +0200

    intermediate project 1

---
+++ src/main/java/pl/sda/intermediate/Customer.java
@@ -0,0 +1,11 @@
+package pl.sda.intermediate;
+
+import java.math.BigDecimal;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private BigDecimal salary;
+}
